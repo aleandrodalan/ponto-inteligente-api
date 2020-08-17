df9c979c40f07e61d8dbef4f37d12755b4e05e8d
Aleandro P. Dalan <aleandro.dalan@loglabdigital.com.br>
2020-08-17 18:20:22 -0400

    Controller cadastro PF e PJ

--- src/main/java/br/com/aleandro/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/com/aleandro/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,9 +1,12 @@
 package br.com.aleandro.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
